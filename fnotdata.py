e703b477d595369a7dc9db786c2c1c2b99c2a684
Nat Picker <tallforasmurf@yahoo.com>
2013-12-16 12:15:52 -0800

    Initialize stub

--- fnotdata.py
+++ fnotdata.py
@@ -1,23 +1,40 @@
+__license__ = '''
+ License (GPL-3.0) :
+    This file is part of PPQT Version 2.
+    PPQT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You can find a copy of the GNU General Public License in the file
+    extras/COPYING.TXT included in the distribution of this program, or see:
+    <http://www.gnu.org/licenses/>.
 '''
-                          FNOTEDATA.PY
+__version__ = "2.0.0"
+__author__  = "David Cortesi"
+__copyright__ = "Copyright 2013, 2014 David Cortesi"
+__maintainer__ = "David Cortesi"
+__email__ = "tallforasmurf@yahoo.com"
 
-Stores footnote info.
+'''
+                          FNOTDATA.PY
 
-Called at fnote_save() to write footnote metadata.
+Defines a class to store the footnote info of one Book. Acts as the Data Model
+for fnotview.py which displays footnote data and offers user controls.
 
-Can be initialized from a metadata file, at fnote_read.
+Methods fnote_load and fnote_save() are registered to read/write footnote
+metadata.
 
-However version 1 metadata (and new files) don't have
-that metadata. So an also be (re-)initialized from the
-Refresh button of the fnote panel. At which time it scans
-the book -- using a textblock generator provided by the book --
-and loads itself with footnote data.
+Books with version 1 metadata and new books don't have that metadata. So can
+also be (re-)initialized from the Refresh button of the Fnote panel which
+calls method refresh(), which scans the book and loads footnote data.
 
-TBS: relationship between this and the fnote panel object,
-presumably the panel gets a ref to this at __init__.
-
-TBS: What services this affords to the fnote panel.
-Basically it acts as the Data Model for the panel's View.
+TBS: What services this affords to fnotview.py.
 
 TBS: Where we put the functions that modify the book text,
 renumbering and moving footnotes. Which object owns the
