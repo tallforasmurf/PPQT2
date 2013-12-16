5572e41ee85fa22297c5768bc0fb4adaec8a708b
Nat Picker <tallforasmurf@yahoo.com>
2013-12-16 12:34:25 -0800

    Added license. Edited prolog.

--- spellcheck.py
+++ spellcheck.py
@@ -1,13 +1,37 @@
+__license__ = '''
+ License (GPL-3.0) :
+    This file is part of PPQT Version 2.
+    PPQT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You can find a copy of the GNU General Public License in the file
+    extras/COPYING.TXT included in the distribution of this program, or see:
+    <http://www.gnu.org/licenses/>.
+'''
+__version__ = "2.0.0"
+__author__  = "David Cortesi"
+__copyright__ = "Copyright 2013, 2014 David Cortesi"
+__maintainer__ = "David Cortesi"
+__email__ = "tallforasmurf@yahoo.com"
+
 '''
                           SPELLCHECK.PY
 
-Implements spellcheck via hunspell.
+Defines the class of objects that provide spellcheck via hunspell.
 
 One of these is created for each open Book because the
 default dict is different per book.
 
-TBS: How do we generate the list of available dicts? Where
-do we store the list? When this object gets an alt-dict tag
-where does it go to validate it and find the files?
+Receives a list of available dicts from the Book.
+
+Implements check(word, alt-tag=None) method used by worddata.py
+to check spelling in a default or alternate dictionary.
 
 '''
