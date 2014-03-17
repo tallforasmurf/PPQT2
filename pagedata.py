4267a571f5be1ea7de37430a8102d5275224f4b9
Nat Picker <tallforasmurf@yahoo.com>
2014-03-17 15:08:46 -0700

    Pagedata.py complete and unit-tested ok. 7usec/call on page_index.

--- pagedata.py
+++ pagedata.py
@@ -98,17 +98,17 @@ and pageview displays all the data in the Pages panel.
 
     page_count()returns the count of pages (for sizing the page table).
 
-    page_at(P)  returns the filename string for the page that contains
+    page_index(P)  returns the row index for the page that contains
                 document offset P, or None if P precedes the first page.
                 This is called every time the edit cursor moves.
 
-    pagename(j) returns the filename string for row j.
+    filename(R) returns the filename string for row R.
 
-    proofers(j) returns the proofer string list for row j.
+    proofers(R) returns the proofer string list for row R.
 
-    folios(j)   returns the folio item list for row j.
+    folios(R)   returns the folio item list for row R.
 
-    set_folios(j, [r,t,p]) update the folio values for row j.
+    set_folios(R, [rule,fmt,nbr]) update the folio values for row j.
 
     Page Boundary Cursors
 
@@ -152,6 +152,16 @@ import metadata
 import editdata
 from PyQt5.QtGui import QTextBlock, QTextCursor
 
+# This regex recognizes a page separator line and captures:
+#   1: image filename -- usually but not always numeric
+#   2: string of proofer names divided by backslashes
+# The compiled regex can be a global because in use, it
+# creates a match object that is private to the caller.
+
+re_line_sep = regex.compile(
+    '''-----File: ([^\\.]+)\\.png---((\\\\[^\\\\]*)+)\\\\-*'''
+    ,regex.IGNORECASE)
+
 class PageData(object):
     def __init__(self, my_book):
         self.my_book = my_book
@@ -164,18 +174,13 @@ class PageData(object):
         self.filename_list = []
         self.folio_list = []
         self.proofers_list = []
-        # Last-returned page row
+        # Flag indicating we have data and are open for business
+        self._active = False
+        # Last-returned position and row
+        self.last_pos = None
         self.last_row = 0
         # Register to read and write metadata
         self.metamgr.register(C.MD_PT, self.read_pages, self.write_pages)
-
-    # This regex recognizes a page separator line and captures:
-    #   1: image filename -- usually but not always numeric
-    #   2: string of proofer names divided by backslashes
-
-    re_line_sep = regex.compile(
-        '''-----File: ([^\\.]+)\\.png---((\\\\[^\\\\]*)+)\\\\-*'''
-        ,regex.IGNORECASE)
 
     #
     # Scan all lines of a new document and create page sep info.
@@ -186,6 +191,7 @@ class PageData(object):
     # This operation creates new data, so we set metadata_modified.
     #
     def scan_pages(self):
+        global re_line_sep
         # first page is Arabic starting at 1
         rule = C.FolioRuleSet
         fmt = C.FolioFormatArabic
@@ -193,8 +199,11 @@ class PageData(object):
         for qtb in self.document.all_blocks() :
             m = re_line_sep.match(qtb.text())
             if m :
+                # capture the image filename
                 fname = m.group(1)
-                plist = m.group(2).split('\\')
+                # secure the proofers as a list, omitting the
+                # null element caused by the leading '\'
+                plist = m.group(2).split('\\')[1:]
                 qtc = QTextCursor(self.document)
                 qtc.setPosition(qtb.position())
                 self.cursor_list.append(qtc)
@@ -207,94 +216,120 @@ class PageData(object):
                 nbr += 1
         if 0 < len(self.cursor_list) : # we found at least 1
             self.my_book.metadata_modified()
+            self._active = True
             self._add_stopper()
 
     # common to scan_pages and read_pages, add a search-stopper
     # to the list of cursors - see page_at() below.
     def _add_stopper(self) :
         qtc = QTextCursor(self.document)
-        qtc.setPosition( self.cursor_list[-1].position() + 1000000 )
+        qtc.setPosition( self.document.characterCount()-1 )
         self.cursor_list.append(qtc)
 
-    # Read the metadata lines and store in our lists. Tdata format is
-    # the same between V1 and V2, 6 space-delimited items, e.g.
+    # Read the metadata lines and store in our lists. This should be called
+    # only once per book. The lists are not cleared at the start so if the
+    # metadata file has multiple PAGETABLEs, they will accumulate. The data
+    # format is the same between V1 and V2, 6 space-delimited items, e.g.
     #     35460 027 \\fmmarshall\\fsmwalb\Scribe 0 0 22
     #
-    # NOTE that unlike worddata we make no allowances here for user
+    # Note that unlike worddata we make no allowances here for user
     # meddling/editing of page data, except for guarding it in a try block.
     #
     def read_pages(self, stream, sentinel, vers, parm):
         for line in metadata.read_to(stream, sentinel):
             try:
-                (P, fn, pfrs, rule, fmt, nbr) = line.split()
+                # throws exception if not exactly 6 items
+                (P, fn, pfrs, rule, fmt, nbr) = line.split(' ')
                 tc = QTextCursor(self.document)
-                tc.setPosition(P)
+                # throws exception if P not an int
+                tc.setPosition(int(P))
+                # check that P was valid document position
+                if int(P) != tc.position() : raise ValueError("Invalid document position")
                 self.cursor_list.append(tc)
                 self.filename_list.append(fn)
                 self.folio_list.append( [int(rule), int(fmt), int(nbr)] )
-                plist = pfs.replace(C.UNICODE_EN_SPACE,' ').split('\\')
+                # get list of proofer strings, dropping opening null string
+                # due to leading backslash.
+                plist = pfrs.replace(C.UNICODE_EN_SPACE,' ').split('\\')[1:]
                 self.proofers_list.append(plist)
             except Exception as who_cares:
                 pagedata_logger.error('invalid line of page metadata:')
-                worddata_logger.error('  "'+line+'"')
+                pagedata_logger.error('  "'+line+'"')
         if 0 < len(self.filename_list) :
+            self._active = True
             self._add_stopper()
 
-    # Write our data as metadata lines.
+    # Write our data as metadata lines. In the page separator lines
+    # the proofers begin with backslash and are delimited with backslash.
+    # For now-inscrutable reasons I kept that (instead of simple join)
+    # format for the metadata file.
 
     def write_pages(self, stream, sentinel):
+        if not self._active : return # don't write an empty section
         stream << metadata.open_line(sentinel)
         for R in range(len(self.filename_list)):
             P = self.cursor_list[R].position()
             fn = self.filename_list[R]
             plist = self.proofers_list[R]
-            pfs = '\\'.join(plist).replace(' ',C.UNICODE_EN_SPACE)
+            # proofer string with leading and delimiting backslash
+            pfs = '\\'+ ('\\'.join(plist).replace(' ',C.UNICODE_EN_SPACE))
             [rule, fmt, nbr] = self.folio_list[R]
             stream << '{0} {1} {2} {3} {4} {5}\n'.format(
                      P,  fn, pfs, rule, fmt, nbr )
         stream << metadata.close_line(sentinel)
 
-    # Use filename_list as the official length; cursor_list has an extra row.
-    def active(self) :
-        return 0 < len(self.filename_list)
-    def page_count(self) :
-        return len(self.filename_list)
-
-    # Return the scan filename matching a document offset. imageview calls
-    # this every time the user moves the cursor, so it needs to be quick. Use
-    # binary search to find the cursor in the cursor_list with the highest
-    # position less than or equal to the given offset.
+    # Return the row index R of the scan page matching a document offset.
+    # Return None if the user is "off the top" in text preceding the first
+    # page. imageview and editview call this every time the user moves the
+    # cursor, so it needs to be quick. Use binary search to find the cursor
+    # in the cursor_list with the highest position less than or equal to the
+    # given offset.
     #
-    # Speed that with heuristics based on these assumptions:
+    # Speed the search with heuristics based on these assumptions:
+    # * we get called from multiple widgets for any one cursor move
     # * the user typically moves the cursor forward, incrementing P
     # * forward or backward, the user typically stays on one page for a while.
-    # So we keep track of the row index of the last-returned page L.
-    # If P > cursor_list[L].position(),
-    #     if P < cursor_list[L+1].position() return filename_list[L]
-    #     else do binary search between L and max
-    # else do binary search between 0 and L
+    # So we keep track of the last-checked position P and also the
+    # row index of the last-returned page R and its filename value F.
+    # If P == last_position : return F
+    # If P > cursor_list[R].position(),
+    #     if P < cursor_list[R+1].position(): return F
+    #     else setup binary search between R and max
+    # else setup binary search between 0 and R
 
-    def page_at(self,P):
-        if P < self.cursor_list[0].position() :
-            self.last_row = 0
-            return None # fiddling around above page 1 text
-        L = self.last_row
-        if P > self.cursor_list[L].position() :
-            if P < self.cursor_list[L+1].position() :
-                return self.filename_list[L] # still in same page span
-            hi = len(self.cursor_list) - 1 # moved on, search in ..
-            lo = L # .. upper part of list
-        else :
-            hi = L # moved back, search in lower part of list
-            lo = 0
-        while lo < hi :
-            mid = (lo + hi)//2
-            if P < self.cursor_list[mid].position() :
-                hi = mid
+    def page_index(self,P):
+        if self._active:
+            if P < self.cursor_list[0].position() :
+                # user is fiddling around in text preceding page 1
+                self.last_row = 0 # must keep a valid row
+                return None
+            if P == self.last_pos : return self.last_row
+            self.last_pos = P
+            R = self.last_row
+            if P >= self.cursor_list[R].position() :
+                if P < self.cursor_list[R+1].position() :
+                    return R # still in same page span
+                hi = len(self.cursor_list) - 1 # moved on, search in ..
+                lo = R # .. upper part of list
             else :
-                lo = mid + 1
-        self.last_row = lo-1
-        return self.filename_list[self.last_row]
+                hi = R # moved back, search in lower part of list
+                lo = 0
+            while lo < hi :
+                mid = (lo + hi)//2
+                if P < self.cursor_list[mid].position() :
+                    hi = mid
+                else :
+                    lo = mid + 1
+            self.last_row = lo-1
+            return self.last_row
+        # else not active
+        return None # no data
+
+    # Use filename_list as the official length; cursor_list has an extra row.
+    def active(self) :
+        return self._active
+    def page_count(self) :
+        return len(self.filename_list)
 
     # Return page values for display by pageview. Note that
     # returning a reference to a list (like the list of folio data)
@@ -304,17 +339,29 @@ class PageData(object):
     #
     # At this time there is no need to modify proofer names.
 
-    def pagename(self, J):
-        return self.filename_list[J]
+    def filename(self, R):
+        try :
+            return self.filename_list[R]
+        except IndexError as I:
+            return ''
 
-    def proofers(self, J):
-        return self.proofers_list[J]
+    def proofers(self, R):
+        try :
+            return self.proofers_list[R]
+        except IndexError as I:
+            return []
 
-    def folios(self, J):
-        return self.folio_list[J]
+    def folios(self, R):
+        try :
+            return self.folio_list[R]
+        except IndexError as I:
+            return []
 
-    def set_folios(self, J, rule = None, fmt = None, nbr = None ):
-        if rule : self.folio_list[J][0] = rule
-        if fmt : self.folio_list[J][1] = fmt
-        if nbr : self.folio_list[J][2] = nbr
-        self.my_book.metadata_modified()
+    def set_folios(self, R, rule = None, fmt = None, nbr = None ):
+        try:
+            if rule : self.folio_list[R][0] = rule
+            if fmt : self.folio_list[R][1] = fmt
+            if nbr : self.folio_list[R][2] = nbr
+            self.my_book.metadata_modified()
+        except IndexError as I :
+            pass
--- tests/pagedata_test.py
+++ tests/pagedata_test.py
@@ -24,4 +24,161 @@ __email__ = "tallforasmurf@yahoo.com"
 '''
 Unit test for pagedata.py
 '''
-assert 'implemented' == 'not yet'
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Unit test module boilerplate stuff
+#
+# set up logging to a stream
+import io
+log_stream = io.StringIO()
+import logging
+logging.basicConfig(stream=log_stream,level=logging.INFO)
+def check_log(text, level):
+    '''check that the log_stream contains the given text at the given level,
+       and rewind the log, then return T/F'''
+    global log_stream
+    level_dict = {logging.DEBUG:'DEBUG',
+                  logging.INFO:'INFO',
+                  logging.WARN:'WARN',
+                  logging.ERROR:'ERROR',
+                  logging.CRITICAL:'CRITICAL'}
+    log_data = log_stream.getvalue()
+    x = log_stream.seek(0)
+    x = log_stream.truncate()
+    return (-1 < log_data.find(text)) & (-1 < log_data.find(level_dict[level]))
+# load a list of one or more lines as a metadata section
+def load_section(mgr, section, line_list, vers=None):
+    stream = metadata.MemoryStream()
+    if vers :
+        stream << '{{VERSION '+vers+'}}\n'
+    stream << metadata.open_line(section)
+    for line in line_list:
+        stream.writeLine(line)
+    stream << metadata.close_line(section)
+    stream.rewind()
+    mm.load_meta(stream)
+# dump all metadata and look at a specified section to see
+# that it contains ALL but ONLY the lines in a list.
+def check_section(mgr, section, line_list):
+    stream = metadata.MemoryStream()
+    mm.write_meta(stream)
+    stream.rewind()
+    while True:
+        line = stream.readLine()
+        if stream.atEnd(): break
+        if line == metadata.open_string(section) : break
+    assert (not stream.atEnd())
+    saved = set()
+    for line in metadata.read_to(stream, section):
+        saved.add(line)
+    assert saved == set(line_list)
+
+# add .. dir to sys.path so we can import ppqt modules which
+# are up one directory level
+import sys
+import os
+my_path = os.path.realpath(__file__)
+test_path = os.path.dirname(my_path)
+ppqt_path = os.path.dirname(test_path)
+sys.path.append(ppqt_path)
+from PyQt5.QtWidgets import QApplication
+app = QApplication(sys.argv)
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+from PyQt5.QtCore import QObject
+import mainwindow
+import metadata
+import constants as C
+import book
+import pagedata
+
+mw = mainwindow.MainWindow()
+the_book = book.Book(mw)
+pagem = the_book.get_page_model()
+mm = the_book.metamgr
+# Load the document with known text
+editm = the_book.get_edit_model()
+doc = '''A12345678
+B12345678
+C12345678
+D12345678'''
+editm.setPlainText(doc)
+# Load page metadata corresponding to those lines
+# Note pf X contains \u2002
+pagemats = '''0 A \pf0\pf1\pf\u2002A\pf3\pf4 1 0 1
+10 B \pf0\pf1\pf\u2002B\pf3\pf4 0 0 2
+20 C \pf0\pf1\pf\u2002C\pf3\pf4 0 0 3
+30 D \pf0\pf1\pf\u2002D\pf3\pf4 0 0 4'''.split('\n')
+load_section(mm, C.MD_PT, pagemats)
+assert pagem.active()
+assert pagem.page_count() == 4
+assert pagem.filename(0) == 'A'
+assert pagem.filename(3) == 'D'
+assert pagem.filename(4) == ''
+assert pagem.proofers(0)[2] == 'pf A'
+assert pagem.proofers(3)[2] == 'pf D'
+assert pagem.proofers(4) == []
+assert pagem.folios(0)[0] == C.FolioRuleSet
+assert pagem.folios(3)[0] == C.FolioRuleAdd1
+assert pagem.folios(3)[2] == 4
+pagem.set_folios(3,rule=C.FolioRuleSet,nbr=9)
+assert pagem.folios(3)[2] == 9
+assert pagem.folios(3)[0] == C.FolioRuleSet
+pagem.set_folios(2,fmt=C.FolioFormatUCRom)
+assert pagem.folios(2) == [C.FolioRuleAdd1,C.FolioFormatUCRom,3]
+line_list = '''0 A \pf0\pf1\pf\u2002A\pf3\pf4 1 0 1
+10 B \pf0\pf1\pf\u2002B\pf3\pf4 0 0 2
+20 C \pf0\pf1\pf\u2002C\pf3\pf4 0 1 3
+30 D \pf0\pf1\pf\u2002D\pf3\pf4 1 0 9'''.split('\n')
+check_section(mm, C.MD_PT, line_list)
+# force various errors in read_pages and check logging
+# wrong number of items
+load_section(mm, C.MD_PT, ['0 A \pf0\pf1\pf\u2002A\pf3\pf4 1 0'])
+assert check_log('invalid line of page metadata:',logging.ERROR)
+load_section(mm, C.MD_PT, ['0 A \pf0\pf1\pf\u2002A\pf3\pf4 1 0 9 9'])
+assert check_log('invalid line of page metadata:',logging.ERROR)
+# page offset off the end of the document or negative
+load_section(mm, C.MD_PT, ['100000 A \pf0\pf1\pf\u2002A\pf3\pf4 0 0 0'])
+assert check_log('invalid line of page metadata:',logging.ERROR)
+load_section(mm, C.MD_PT, ['-1 A \pf0\pf1\pf\u2002A\pf3\pf4 0 0 0'])
+assert check_log('invalid line of page metadata:',logging.ERROR)
+# Load the document from an actual book and scan it.
+# Purely for test purposes, re-call the __init__ function to
+# clear the existing data.
+pagem.__init__(the_book)
+assert not pagem.active()
+
+fx_path = test_path+'/Files/'
+sb_path = fx_path+'small_book.txt'
+sb = open(sb_path,'r',encoding='Latin-1')
+editm.setPlainText(sb.read())
+pagem.scan_pages()
+assert pagem.active()
+# following depends on contents of small_book.txt:
+# 27 pages, last proofer list ends in bazongas!
+# note it has one line of text preceding page 1.
+assert pagem.page_count() == 27
+assert pagem.filename(26) == '027'
+assert pagem.proofers(26)[3] == 'bazongas!'
+# Test basics of page_index()
+assert pagem.page_index(0) is None
+pz = editm.lastBlock().position() + editm.lastBlock().length() - 2
+p1 = editm.findBlockByLineNumber(1).position()
+for j in range(p1) :
+    assert pagem.page_index(j) is None
+assert pagem.page_index(p1) == 0
+assert pagem.page_index(pz) == 26
+# time a bunch of calls to page_index using a sequence of
+# random positions in p1..pz that have some locality of reference.
+import timeit
+import random
+def jumper():
+    global pagem,pz,p1
+    hops = random.sample(range(200),20) # 20 little moves
+    jumps = random.sample(range(pz-p1),50) # 50 bigger moves
+    for J in jumps:
+        for H in hops:
+            x = pagem.page_index(J+H)
+#pi_time = timeit.timeit(jumper,number=10)
+#print(pi_time)
+# time to execute 10,000 calls: 0.073 sec, or 0.0000073 sec/call,
+# or 7usec call. This will do at least for now.
