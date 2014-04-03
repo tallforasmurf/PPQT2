8a4ff296d8c3bb7dcc6b3b6620974fce61265b92
Nat Picker <tallforasmurf@yahoo.com>
2014-04-03 12:30:07 -0700

    implement the scale() method also use default size constant

--- fonts.py
+++ fonts.py
@@ -46,8 +46,7 @@ don't have to know about the QFont/QFontInfo API.
     get_general() returns the QFont for the selected general font
         at a specified or default size.
 
-    TODO: FIGURE OUT BEST API FOR SCALE +/- points? abs % (of what)
-    scale() scales the size of a font up or down and returns the
+    scale() scales the size of a font up or down 1 point and returns the
         modified QFont.
 
     get_size() returns the integer size of a QFont (presumably,
@@ -57,36 +56,56 @@ don't have to know about the QFont/QFontInfo API.
 '''
 import logging
 fonts_logger = logging.getLogger(name='fonts')
+import constants as C
 
 from PyQt5.QtGui import (QFont, QFontInfo, QFontDatabase)
+POINT_SIZE_MINIMUM = 6 # smallest point size for zooming
+POINT_SIZE_MAXIMUM = 48 # largest for zooming
 
+# TODO replace this temporary font-getting kludge
 font_db = QFontDatabase()
 qf_general = font_db.systemFont(QFontDatabase.GeneralFont)
 families = font_db.families()
 libmono = [family for family in families if 'Liberation Mono' in family]
 couriermono = [family for family in families if 'Courier New' in family]
 if libmono :
-    qf_mono = font_db.font(libmono[0],'Regular',16)
+    qf_mono = font_db.font(libmono[0],'Regular',C.DEFAULT_POINT_SIZE)
     dbg = qf_mono.family()
 elif couriermono :
-    qf_mono = font_db.font(couriermono[0],'Regular',16)
+    qf_mono = font_db.font(couriermono[0],'Regular',C.DEFAULT_POINT_SIZE)
 else:
     qf_mono = font_db.systemFont(QFontDatabase.FixedFont)
 
 def initialize(settings):
+    # TODO: Implement
     fonts_logger.debug('Fonts initializing')
 
-def get_fixed(point_size=16):
+def get_fixed(point_size=C.DEFAULT_POINT_SIZE):
+    # TODO: implement point size arg
     return qf_mono
 
-def get_general(point_size=16):
+def get_general(point_size=C.DEFAULT_POINT_SIZE):
+    # TODO: implement point size arg
     return qf_general
 
 def get_size(qf):
     return qf.pointSize()
 
-def scale(qf, pct) :
-    fonts_logger.error('Unimplemented:scale')
+# Called from editview and others to zoom a current
+# font up or down 1 point, depending on the key,
+# which should be one of constants.KEYS_ZOOM.
+# Limit the font size range.
+
+def scale(zoom_key, qfont ) :
+    if zoom_key in C.KEYS_ZOOM :
+        pts = qf.pointSize()
+        pts += (-1 if zoom_key == C.CTL_MINUS else 1)
+        if (pts >= POINT_SIZE_MINIMUM) and (pts <= POINT_SIZE_MAXIMUM) :
+            qf.setPointSize(pts)
+        else:
+            fonts_logger.error('rejecting zoom to {0} points'.format(pts))
+    else:
+        fonts_logger.error('ignoring non-zoom key argument')
     return qf
 
 def choose_font(mono=True, parent=None):
