cdfd4b2dc8cebc28697a9f2009080b00c37b26bb
Nat Picker <tallforasmurf@yahoo.com>
2014-04-09 15:52:30 -0700

    Clean up API commentary and add all def's

--- colors.py
+++ colors.py
@@ -31,37 +31,47 @@ scanno highlights and spelling-error highlights and current line.
 
 The following are interrogated by an editview:
 
+    initialize(settings) Retrieve color choices from saved settings,
+                         or set the defaults.
+
+    shutdown(settings)   Save color choices in app settings.
+
     get_modified_color() Return a QColor to use for the document name
                          when it is modified.
+
+    set_modified_color(qcolor) Set the color to use in the document name.
 
     get_scanno_format()  Return a QTextCharFormat with the chosen
                          style and color for scanno highlighting.
 
+    set_scanno_format(fmt) Set the scanno highlight QTextCharFormat
+
     get_spelling_format() Return a QTextCharFormat with the chosen
                          style and color for spelling-error highlighting.
+
+    set_spelling_fmt(fmt) set the spelling-error highlight format.
 
     get_current_line_brush() Return a QBrush to use for the background
                         color of the current line.
 
+    set_current_line_color(qcolor) set the current-line highlight brush to a color
+
 The following are used by a TODO preferences dialog:
 
-    choose_color(message) Present a QColorDialog to the user and
-                         return a QColor or None if cancelled.
+    choose_color(message) Present a QColorDialog to the user headed by
+                         the given message, return a QColor or None if cancelled.
 
     make_format( color, line_type = QTextCharFormat.NoUnderline )
                         Create a QTextCharFormat for either a background
                         color (default line_type) or for an underline of
                         the specified type and QColor.
 
-    set_scanno_format(fmt) Set the scanno highlight QTextFormat
-
-    set_spelling_fmt(fmt) set the spelling-error highlight format
-
-    set_current_line_format(fmt) set the current-line highlight format
 
 '''
 
 from PyQt5.QtGui import QBrush, QColor, QTextCharFormat, QTextBlockFormat
+import logging
+colors_logger = logging.getLogger(name='colors')
 
 MD_COLOR = QColor('red')
 SC_COLOR = QColor('thistle')
@@ -69,13 +79,39 @@ SP_COLOR = QColor('lightpink')
 CL_COLOR = QColor('#FAFAE0') # very light yellow
 
 # TODO temps
+def choose_color(message):
+    colors_logger.error('Unimplemented:choose color')
+
+def make_format( qcolor = QColor('Black'), line_type = QTextCharFormat.NoUnderline):
+    qtcf = QTextCharFormat()
+    if line_type == QTextCharFormat.NoUnderline:
+        # make a background-color format
+        qtcf.setBackground(QBrush(qcolor))
+    else :
+        # make an underline format
+        qtcf.setUnderlineColor(qcolor)
+        qtcf.setUnderlineStyle(line_type)
+    return qtcf
+
+def initialize(settings):
+    colors_logger.error('Unimplemented:initialize')
+
+def shutdown(settings):
+    colors_logger.error('Unimplemented:shutdown')
+
 def get_modified_color():
     return MD_COLOR
+
+def set_modified_color():
+    colors_logger.error('Unimplemented:set modified color')
 
 def get_scanno_format():
     qtcf = QTextCharFormat()
     qtcf.setBackground(QBrush(SC_COLOR))
     return qtcf
+
+def set_scanno_format(qtcf):
+    colors_logger.error('Unimplemented:set scanno format')
 
 def get_spelling_format():
     qtcf = QTextCharFormat()
@@ -83,12 +119,11 @@ def get_spelling_format():
     qtcf.setUnderlineColor(SP_COLOR)
     return qtcf
 
+def set_spelling_format(qtcf):
+    colors_logger.error('Unimplemented:set spelling format')
+
 def get_current_line_brush():
     return QBrush(CL_COLOR)
 
-def set_scanno_format(qtcf):
-    pass
-def set_spelling_format(qtcf):
-    pass
 def set_current_line_format(qtdf):
-    pass
+    colors_logger.error('Unimplemented:set scanno format')
