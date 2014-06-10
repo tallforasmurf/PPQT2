bc34dc9e881dd3bfc491d29fa5b1efdc45a82853
Nat Picker <tallforasmurf@yahoo.com>
2014-06-10 12:12:05 -0700

    Bring up to date, add more tests

--- tests/dictionaries_test.py
+++ tests/dictionaries_test.py
@@ -53,23 +53,42 @@ my_path = os.path.realpath(__file__)
 test_path = os.path.dirname(my_path)
 ppqt_path = os.path.dirname(test_path)
 sys.path.append(ppqt_path)
-
-import dictionaries
 files_path = os.path.join(test_path, 'Files')
-# Check the dictionary global part
-dictionaries.set_dict_path(files_path)
-dictionaries.set_extras_path(files_path)
+# Create an app and empty settings
+from PyQt5.QtWidgets import QApplication
+app = QApplication(sys.argv)
+import constants as C
+app.setOrganizationName("PGDP")
+app.setOrganizationDomain("pgdp.net")
+app.setApplicationName("PPQT2")
+from PyQt5.QtCore import QSettings
+settings = QSettings()
+settings.setValue("dictionaries/path",files_path)
+settings.setValue("dictionaries/default_tag","en_GB")
+import dictionaries
+dictionaries.initialize(settings)
 assert files_path == dictionaries.get_dict_path()
-assert files_path == dictionaries.get_extras_path()
-# There are just 2 dicts in Tests/Files:
-expect_tags = ['en_US','fr_FR']
+assert 'en_GB' == dictionaries.get_default_tag()
+
+# There are 3 dicts in Tests/Files:
+expect_tags = ['en_US','en_GB','fr_FR']
 tag_list = dictionaries.get_tag_list('')
 for tag in expect_tags:
     assert tag in tag_list
 assert len(expect_tags) == len(tag_list.keys())
-# The above should produce two log messages
-assert check_log("aff but not",logging.ERROR)
+# Create a mismatched dic/aff pair
+dic_path = os.path.join(files_path,'foobar.dic')
+open(dic_path,'w')
 tag_list = dictionaries.get_tag_list('')
+assert check_log(".dic but not",logging.ERROR)
+os.remove(dic_path) # clean up Files
+aff_path = os.path.join(files_path,'foobar.aff')
+open(aff_path,'w')
+tag_list = dictionaries.get_tag_list('')
+assert check_log(".aff but not",logging.ERROR)
+os.remove(aff_path) # clean up Files
+# "skipping" should appear if we check a path twice
+tag_list = dictionaries.get_tag_list(files_path)
 assert check_log("Skipping",logging.INFO)
 # Check the spellcheck object part
 SP = dictionaries.Speller("en_US",dictionaries.get_dict_path())
