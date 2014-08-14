1d6fb008d846d5afff26d5dd19b83fdb036bccf5
David E. Cortesi <dcortesi@Silver-streak.local>
2014-08-14 15:42:43 -0700

    Update commentary. re-code iterators to use isValid and more complete error checks.

--- editdata.py
+++ editdata.py
@@ -38,21 +38,21 @@ Externally-available functions, some from QTextDocument, some unique
     full_text()          get a reference to a (possibly very large) Python string
                          containing the document contents. Used by find, save.
 
+    all_blocks()         iterator returning each QTextBlock in turn.
+
+    a_to_z_blocks(a, z)  iterator returning QTextBlocks from a to z inclusive.
+
     all_lines()          iterator returning the text of each QTextBlock as a
                          Python string in turn: "for line in all_lines()"
 
     a_to_z_lines(a, z)   iterator returning text of each line from a to z
-                         inclusive.
+                         inclusive, a and z being text block numbers.
 
     z_to_a_lines(a, z)   iterator returning the text of each line from z
                          working numerically back to a.
 
     cursor_lines(c)      iterator returning the text of each line spanned
                          by the selection of a QTextCursor c.
-
-    all_blocks()         iterator returning each QTextBlock in turn.
-
-    a_to_z_blocks(a, z)  iterator returning QTextBlocks from a to z inclusive.
 
 '''
 from PyQt5.QtGui import (
@@ -70,9 +70,9 @@ class Document(QTextDocument):
     # TODO study qtdocument and do many overrides - resource? redos?
     def __init__(self, my_book):
         super().__init__(parent = my_book)
-        # set up cached copy of document text, see full_text()
+        # Initialize slot for cached copy of document text, see full_text()
         self._text = None
-        self.contentsChanged.connect(self.text_modified)
+        self.contentsChanged.connect(self._text_modified)
 
         # TODO do I want to customize the layout?
         self.setDocumentLayout(QPlainTextDocumentLayout(self))
@@ -95,48 +95,95 @@ class Document(QTextDocument):
             self._text = self.toPlainText()
         return self._text
 
-    def text_modified(self):
+    def _text_modified(self):
         self._text = None
 
-    # The following functions create iterators over sequences of QTextBlocks,
-    # producing a Python string of the contents of each. They gives read
-    # access to sequences of lines, as for a census or when parsing text for
-    # reflow.
+    # The following functions return iterators over sequences of QTextBlocks,
+    # returning the QTextBlocks themselves. These are only used when access
+    # to QTextBlock methods are needed. pagedata for example needs to know
+    # the .position() of the textblocks that contain page separator lines.
     #
-    # Note that findBlockByNumber(x)==end() for any x that is invalid,
+    # Note that, A, a QTextDocument is never completely empty, it always
+    # contains at least 1 QTextBlock: .blockCount() >= 1 and .begin() != .end()
+    #
+    # Note B, findBlockByNumber(x)==end() for any x that is invalid,
     # whether negative or greater than blockCount.
     #
     # 1. The whole document from top to bottom, as for a census.
     #
-    def all_lines(self):
-        tb = self.findBlockByNumber(0)
+    def all_blocks(self):
+        tb = self.begin()
         while tb != self.end():
+            yield tb
+            tb = tb.next()
+    #
+    # 2. A range of lines from block #a to #z inclusive, guarding against
+    # invalid ranges. We expect that a and z index existing blocks,
+    # i.e. 0 <= a < .blockCount, 0 <= z < .blockCount, a <= z.
+    #
+    # The initial tests guarantee that 0 <= a < .blockCount and a <= z,
+    # but we don't check that z < .blockCount; if not, tbx == self.end().
+    # So the loop can start with a yield, but it needs two termination
+    # checks: have we yielded the valid block z, and if not, have we
+    # reached the end of valid blocks?
+    #
+    def a_to_z_blocks(self, a, z):
+        tb = self.findBlockByNumber(a)
+        tbx = self.findBlockByNumber(z)
+        if a <= z and tb.isValid() :
+            while True :
+                yield tb # loop invariant: tb is valid and <= tbx
+                if tb == tbx : break
+                tb = tb.next()
+                if not tb.isValid() : break
+
+    # The following functions create iterators over sequences of QTextBlocks,
+    # but return the Python string of the contents of each. They gives read
+    # access to sequences of lines, as for a census or when parsing text for
+    # reflow.
+    #
+    # 1. The whole document from top to bottom, as for a census.
+    #
+    def all_lines(self):
+        tb = self.begin()
+        while tb.isValid():
             yield tb.text()
             tb = tb.next()
     #
     # 2. A range of lines from LINE NUMBER a to z inclusive, guarding
-    # against invalid ranges and a completely empty doc.
+    # against invalid ranges and a completely empty doc. Line #s are
+    # origin-1, textblock #s origin-0. Same comments as for a_to_z_blocks.
     #
     def a_to_z_lines(self, a, z):
         tb = self.findBlockByNumber(a - 1)
-        tbx = self.findBlockByNumber(z - 1) # if invalid, self.end()
-        if a <= z and a > 0 and tb != self.end() :
+        tbx = self.findBlockByNumber(z - 1)
+        if a <= z and tb.isValid() :
             while True:
-                yield tb.text()
+                yield tb.text() # loop invariant: here tb is valid and <= tbx
                 if tb == tbx : break
                 tb = tb.next()
+                if not tb.isValid() : break
+
     #
     # 3. A range of lines from LINE NUMBER z to a inclusive in reverse
-    # sequence.
+    # sequence. If z is not valid we start with lastBlock(). However
+    # if a is invalid, we do not know how far to go. Return all lines
+    # down to the first? Return just one? We opt for the latter.
     #
     def z_to_a_lines(self, a, z):
         tb = self.findBlockByNumber(z - 1)
+        if not tb.isValid() :
+            tb = self.lastBlock()
+            z = tb.blockNumber() + 1
         tbx = self.findBlockByNumber(a - 1)
-        if a <= z and a > 0 and tb != self.end() :
-            while True:
-                yield tb.text()
-                if tb == tbx : break
-                tb = tb.previous()
+        if (a > z) or (not tbx.isValid()) :
+            tbx = tb
+            a = z
+        # here we are sure that both tb.isValid && tbx.isValid
+        while True:
+            yield tb.text()
+            if tb == tbx : break
+            tb = tb.previous()
     #
     # 4. A range of lines included in the selection of a text cursor.
     # Includes the first and last lines even if the selection does not
@@ -149,29 +196,3 @@ class Document(QTextDocument):
         a = self.findBlock(ca).blockNumber()
         z = self.findBlock(cz).blockNumber()
         return self.a_to_z_lines( a+1, z+1 )
-
-    # The following functions return iterators over sequences of
-    # QTextBlocks, returning the QTextBlocks themselves. These are
-    # only used when access to QTextBlock methods are needed.
-    # pagedata for example needs to know the .position() of the
-    # textblocks that contain page separator lines.
-    #
-    # 1. The whole document from top to bottom, as for a census.
-    #
-    def all_blocks(self):
-        tb = self.findBlockByNumber(0)
-        while tb != self.end():
-            yield tb
-            tb = tb.next()
-    #
-    # 2. A range of lines from block #a to #z inclusive, guarding
-    # against invalid ranges.
-    #
-    def a_to_z_blocks(self, a, z):
-        tb = self.findBlockByNumber(a)
-        tbx = self.findBlockByNumber(z)
-        if a >= 0 and a <= z and tb != self.end() :
-            while True :
-                yield tb
-                if tb == tbx : break
-                tb = tb.next()
